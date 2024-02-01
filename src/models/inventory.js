b4d7faf164a3d0ef8ea1e0678dbe5e45ea04254c
vikkiakavc <vschaudhary2001@gmail.com>
2024-02-01 19:58:36 +0530

    set up routers for inventories

---
+++ src/models/inventory.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+module.exports = (sequelize, DataTypes) => {
+  const Inventory = sequelize.define("inventory", {
+    name: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allownull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allownull: false,
+      validate: {
+        min: 0,
+        isDecimal: true,
+      },
+    },
+    supplier: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+  });
+  Inventory.beforeCreate(async (item, option) => {
+    item.supplier = item.supplier.trim().toLowerCase();
+  });
+  return Inventory;
+};
